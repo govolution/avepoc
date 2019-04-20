16d3f7483d5a25c595ab7daf01a49b337f60f252
Daniel Sauder <daniel.sauder@web.de>
2019-04-20 23:17:34 +0200

    cleaned up

--- isvm.c
+++ isvm.c
@@ -1,5 +1,6 @@
 // source: https://www.pelock.com/articles/anti-reverse-engineering-malware-vs-antivirus-software
 // some changes needed ;)
+// tested with vmware workstation & virtualbox
 
 #include <windows.h>
 #include <stdio.h>
@@ -10,26 +11,18 @@ BOOL IsVM()
     int i;
     char szBuffer[1024];
     char *szProducts[] = { "VMWARE", "VBOX", "VIRTUAL", "VMware" };
-//printf("count of szProducts: %d\n",_countof(szProducts));
     DWORD dwSize = sizeof(szBuffer) - 1;
 
     if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, "SYSTEM\\ControlSet001\\Services\\Disk\\Enum", 0, KEY_READ, &hKey) == ERROR_SUCCESS)
     {
         RegQueryValueEx(hKey, "0", NULL, NULL, (unsigned char *)szBuffer, &dwSize );
-        printf("szbuffer test 1: %s\n",szBuffer);
-        //if (RegQueryValueEx(hKey, "0", NULL, NULL, (unsigned char *)szBuffer, &dwSize ) == ERROR_SUCCESS)
+        for (i = 0; i < _countof(szProducts); i++)
         {
-            printf("count of szProducts: %d\n",_countof(szProducts));
-            for (i = 0; i < _countof(szProducts); i++)
+            if (strstr(szBuffer, szProducts[i]))
             {
-                if (strstr(szBuffer, szProducts[i]))
-                {
-                    RegCloseKey(hKey);
-                    return TRUE;
-                }
-                printf("search %d\n",i);
-		printf("szBuffer: %s, szPRoductsi:%s\n",szBuffer,szProducts[i]);
-        }
+                RegCloseKey(hKey);
+                return TRUE;
+            }
     }
 
     RegCloseKey(hKey);
